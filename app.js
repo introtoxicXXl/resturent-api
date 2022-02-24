7849345fba2b3db8d08db168e844d01ebff095ac
introtoxicXXl <minhajulabedin648@gmail.com>
2022-02-24 23:38:03 +0600

    error handaling

--- app.js
+++ app.js
@@ -11,17 +11,18 @@ const loadMeal = () => {
 const displayMeal = meals => {
   // console.log(meals)
   const mealCard = document.getElementById('meal-card');
+  mealCard.textContent = '';
   meals.forEach(meal => {
     console.log(meal)
     const div = document.createElement('div');
+    
     div.className = 'col';
     div.innerHTML = `
         <div class="card h-100">
-             <img src="..." class="card-img-top" alt="...">
+             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-              content. This content is a little bit longer.</p>
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0,150)}</p>
               <button href="#" class="btn btn-primary">Go somewhere</button>
           </div>
         </div>
